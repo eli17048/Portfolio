3cf379553a6e5f8e2759430dc23de02a5ed98618
eli17048 <andrea.elias16@gmail.com>
2020-06-05 21:10:33 -0600

    Updates

---
+++ precache-manifest.f861eefd492468d5d99dc9ca191f1b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44f8d63b4d216b6e377582c3ebd1d10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f070411125f7345c964",
+    "url": "./static/css/main.b787663f.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "./static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f070411125f7345c964",
+    "url": "./static/js/main.98514e15.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  }
+]);
